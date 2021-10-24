458a77b570a45e29761d9e55477bd8d843dc2fb4
Javier Guillén <javierghdaw@gmail.com>
2021-10-24 18:05:11 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+	<?php
+	
+	print "Hasta luego desarrollador!!!"
+	
+	?>
+	
+</body>
+</html>
